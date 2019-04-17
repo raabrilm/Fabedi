2da25ae255b2f64612a19ea5373e1e7bbeeb7fa9
raabrilm <abrilmora@gmail.com>
2019-04-17 10:45:25 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyAzzBG1HxT9QQJC0b2JVOScDv8x-7NEmrE",
+    apiKey: "",
     authDomain: "fabedi-shop.firebaseapp.com",
     databaseURL: "https://fabedi-shop.firebaseio.com",
     projectId: "fabedi-shop",
     storageBucket: "fabedi-shop.appspot.com",
-    messagingSenderId: "944958130567"
+    messagingSenderId: ""
   }
 };
 
