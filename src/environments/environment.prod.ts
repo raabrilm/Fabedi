435116b844dc95bb6fa641c9beb2b2780e0a4862
raabrilm <abrilmora@gmail.com>
2019-04-17 10:21:37 -0500

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAzzBG1HxT9QQJC0b2JVOScDv8x-7NEmrE",
+    authDomain: "fabedi-shop.firebaseapp.com",
+    databaseURL: "https://fabedi-shop.firebaseio.com",
+    projectId: "fabedi-shop",
+    storageBucket: "fabedi-shop.appspot.com",
+    messagingSenderId: "944958130567"
+  }
+};
+
